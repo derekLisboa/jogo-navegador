a955c6c92948846724356a55029b9a1a77993ec2
Derek Lisboa <derekmlisboa@gmail.com>
2025-02-05 21:49:16 -0300

    Adiçao do codigo Game e estilizaçao

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../Game/Game.css'
 
+
 function Game({ players, setPlayers }) {
   const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
   const [letter, setLetter] = useState('');
